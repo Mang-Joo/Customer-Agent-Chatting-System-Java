edf15b2fbe57604446a4bc45ed21e4f321e6523b
Mang-Joo <wjwan0915@gmail.com>
2025-02-01 10:46:09 +0900

    Init

---
+++ src/main/java/io/github/mangjoo/customer_agent_chat_service/CustomerAgentChatServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.mangjoo.customer_agent_chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAgentChatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAgentChatServiceApplication.class, args);
+    }
+
+}
