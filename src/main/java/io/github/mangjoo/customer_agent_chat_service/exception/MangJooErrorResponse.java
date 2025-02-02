cf0ed214efaeaed6ac724ca137c7310d4351c386
Mang-Joo <wjwan0915@gmail.com>
2025-02-02 10:43:28 +0900

    :recycle: refactor: Change to record of ErrorResponse Class

--- src/main/java/io/github/mangjoo/customer_agent_chat_service/exception/MangJooErrorResponse.java
+++ src/main/java/io/github/mangjoo/customer_agent_chat_service/exception/MangJooErrorResponse.java
@@ -1,15 +1,8 @@
 package io.github.mangjoo.customer_agent_chat_service.exception;
 
 
-import lombok.Getter;
-
-@Getter
-public class MangJooErrorResponse {
-    private String errorCode;
-    private String message;
-
-    public MangJooErrorResponse(String errorCode, String message) {
-        this.errorCode = errorCode;
-        this.message = message;
-    }
+public record MangJooErrorResponse(
+        String errorCode,
+        String message
+) {
 }
