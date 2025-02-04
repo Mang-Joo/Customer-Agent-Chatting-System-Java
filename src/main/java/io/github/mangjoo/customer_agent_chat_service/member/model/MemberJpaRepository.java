389a6d67a8b290c5c4235baf30c40cac056662ed
Mang-Joo <wjwan0915@gmail.com>
2025-02-04 10:16:54 +0900

    :sparkles: feat: Add Email check when member register

--- src/main/java/io/github/mangjoo/customer_agent_chat_service/member/model/MemberJpaRepository.java
+++ src/main/java/io/github/mangjoo/customer_agent_chat_service/member/model/MemberJpaRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
