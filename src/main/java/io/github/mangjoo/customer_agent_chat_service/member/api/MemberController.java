23436d2f1de06af299406c1a2234ad66917cbf2e
Mang-Joo <wjwan0915@gmail.com>
2025-02-04 10:20:30 +0900

    :memo: chore : Member Register Swagger

--- src/main/java/io/github/mangjoo/customer_agent_chat_service/member/api/MemberController.java
+++ src/main/java/io/github/mangjoo/customer_agent_chat_service/member/api/MemberController.java
@@ -1,25 +1,47 @@
 package io.github.mangjoo.customer_agent_chat_service.member.api;
 
+import io.github.mangjoo.customer_agent_chat_service.exception.MangJooErrorResponse;
 import io.github.mangjoo.customer_agent_chat_service.member.api.request.MemberRegisterRequest;
 import io.github.mangjoo.customer_agent_chat_service.member.api.request.MemberRegisterResponse;
 import io.github.mangjoo.customer_agent_chat_service.member.model.Member;
 import io.github.mangjoo.customer_agent_chat_service.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Tag(name = "Member API", description = "Member API")
 public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(
+            summary = "Register Member API",
+            description = "Register Member",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Member Registered Successfully"),
+                    @ApiResponse(responseCode = "400", description = "Member Already Exists or Invalid Request",
+                            content = @Content(schema = @Schema(implementation = MangJooErrorResponse.class))
+                    )
+            }
+    )
     public ResponseEntity<MemberRegisterResponse> register(
+            @Parameter(description = "Register Member Request", required = true)
             @Valid @RequestBody MemberRegisterRequest request
     ) {
         Member registeredMember = memberService.register(request.toMember());
--- src/main/java/io/github/mangjoo/customer_agent_chat_service/member/api/request/MemberRegisterRequest.java
+++ src/main/java/io/github/mangjoo/customer_agent_chat_service/member/api/request/MemberRegisterRequest.java
@@ -1,12 +1,42 @@
 package io.github.mangjoo.customer_agent_chat_service.member.api.request;
 
 import io.github.mangjoo.customer_agent_chat_service.member.model.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
+@Schema(description = "Register Member Request Object")
 public record MemberRegisterRequest(
-        @NotNull @NotBlank String email,
-        @NotNull @NotBlank String password,
+        @Schema(
+                description = "Register Member Email (Login Email)",
+                example = "MangJoo@gmail.com",
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
+        @NotNull(message = "Email is required.")
+        @NotBlank(message = "Email is required.")
+        @Email(message = "It's not email form.")
+        String email,
+
+        @Schema(
+                description = "Register Member Password",
+                example = "MangJoo1234",
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
+        @NotNull(message = "Password is required.")
+        @NotBlank(message = "Password is required.")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+                message = "Password have to contain at least one letter and one number, and be longer than 8 characters."
+        )
+        String password,
+
+        @Schema(
+                description = "Register Member Name",
+                example = "MangJoo",
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
         @NotNull @NotBlank String name
 ) {
     public Member toMember() {
