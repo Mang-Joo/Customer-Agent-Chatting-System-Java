a187794d89a7d19cc2c03a7e4dd935cf60e035e3
Mang-Joo <wjwan0915@gmail.com>
2025-02-04 10:20:06 +0900

    :sparkles: feat: Add Validation Exception Handler

--- src/main/java/io/github/mangjoo/customer_agent_chat_service/exception/CustomExceptionHandler.java
+++ src/main/java/io/github/mangjoo/customer_agent_chat_service/exception/CustomExceptionHandler.java
@@ -2,6 +2,7 @@ package io.github.mangjoo.customer_agent_chat_service.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +14,16 @@ public class CustomExceptionHandler {
     public ResponseEntity<MangJooErrorResponse> handleMangJooException(MangJooException e) {
         log.error("MangJooException: {}", e.getMessage());
         return e.toErrorResponse();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MangJooErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException: {}", e.getMessage());
+
+        e.getBindingResult().getFieldErrors().forEach(fieldError ->
+                log.error("Field: {}, Message: {}", fieldError.getField(), fieldError.getDefaultMessage())
+        );
+
+        return new MangJooException(ErrorCode.BAD_REQUEST, e.getBindingResult().getFieldError().getDefaultMessage()).toErrorResponse();
     }
 }
