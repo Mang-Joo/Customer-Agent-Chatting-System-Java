8ba66b36bb8622e44f7b299b3d6201c95464678e
Mang-Joo <wjwan0915@gmail.com>
2025-02-05 10:34:24 +0900

    :sparkles: feat: Add Redis Template for Check Duplicate Sessions

--- src/main/java/io/github/mangjoo/customer_agent_chat_service/member/repository/MemberRepository.java
+++ src/main/java/io/github/mangjoo/customer_agent_chat_service/member/repository/MemberRepository.java
@@ -5,13 +5,17 @@ import io.github.mangjoo.customer_agent_chat_service.exception.MangJooException;
 import io.github.mangjoo.customer_agent_chat_service.member.model.Member;
 import io.github.mangjoo.customer_agent_chat_service.member.model.MemberJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
     private final MemberJpaRepository memberJpaRepository;
+    private final RedisTemplate<Object, Object> redisTemplate;
 
     @Transactional(readOnly = true)
     public Member findByEmail(String email) {
@@ -35,4 +39,21 @@ public class MemberRepository {
         return memberJpaRepository.findById(userId)
                 .orElseThrow(() -> new MangJooException(ErrorCode.BAD_REQUEST, "Member not found"));
     }
+
+    @Transactional
+    public void saveSession(String sessionKey, String sessionValue) {
+        redisTemplate.opsForHash().put(sessionKey, sessionKey, sessionValue);
+        redisTemplate.expire(sessionKey, Duration.ofMinutes(30));
+    }
+
+    @Transactional(readOnly = true)
+    public String getSession(String sessionKey) {
+        return (String) redisTemplate.opsForHash().get(sessionKey, sessionKey);
+    }
+
+    @Transactional
+    public void renewSession(String sessionKey) {
+        redisTemplate.expire(sessionKey, Duration.ofMinutes(30));
+    }
+
 }
