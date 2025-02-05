e68cf2336f2af9b0b0505413211a3094291959c1
Mang-Joo <wjwan0915@gmail.com>
2025-02-05 10:34:55 +0900

    :sparkles: feat: EnableRedisHttpSession

--- src/main/java/io/github/mangjoo/customer_agent_chat_service/CustomerAgentChatServiceApplication.java
+++ src/main/java/io/github/mangjoo/customer_agent_chat_service/CustomerAgentChatServiceApplication.java
@@ -2,8 +2,10 @@ package io.github.mangjoo.customer_agent_chat_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class CustomerAgentChatServiceApplication {
 
     public static void main(String[] args) {
